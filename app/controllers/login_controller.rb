b048597d19d78ebe157b1aef190ab01f171bf28e
Zach Cowen <cowen.30@osu.edu>
2021-09-26 22:07:03 -0400

    only select necessary attributes

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,8 +5,10 @@ class LoginController < ApplicationController
 	skip_before_action :require_login, only: %i[create]
 
 	def create
-		@user = User.find_by('lower(email) = ?', user_params[:email].downcase)
+		@user_query = User.where('lower(email) = ?', user_params[:email].downcase).select(:id, :password_digest)
+		@user = @user_query.first
 		if @user && @user.authenticate(user_params[:password])
+			@user = @user_query.reselect(:id, :first_name, :last_name).first
 			render :create, locals: { token: token(@user) }
 		else
 			render json: { errors: [ 'Invalid username or password' ] }, status: :unprocessable_entity
