ef358b25248a50de4beab4d6f5006944c4e6baf1
Zach Cowen <cowen.30@osu.edu>
2021-10-20 22:27:33 -0400

    add other password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 	validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
 	validates :email, uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 8 }, confirmation: true
+	validates :password,
+		length: { minimum: 8 },
+		format: { with: /[A-Z]/, message: 'must contain at least one uppercase character' },
+		confirmation: true
+	validates :password, format: { with: /[!@#$%^&*]/, message: 'must contain at least one special character' }
 	has_secure_password
 
 	def to_builder
