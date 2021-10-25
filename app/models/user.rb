e1ba01b2ca48f2bd197347206daafe1743ab14c7
Zach Cowen <cowen.30@osu.edu>
2021-10-24 21:37:22 -0400

    only validate user params on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-	validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+	validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true, on: :create
 	validates :email, uniqueness: { case_sensitive: false }
 	validates :password,
 		length: { minimum: 8 },
 		format: { with: /[A-Z]/, message: 'must contain at least one uppercase character' },
-		confirmation: true
-	validates :password, format: { with: /[!@#$%^&*]/, message: 'must contain at least one special character' }
+		confirmation: true,
+		if: :password_digest_changed?
+	validates :password, format: { with: /[!@#$%^&*]/, message: 'must contain at least one special character' }, if: :password_digest_changed?
 	has_secure_password
 
 	def to_builder
